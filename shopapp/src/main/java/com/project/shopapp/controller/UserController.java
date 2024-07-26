0129b8a534abced2810526ac3080bec92629265d
lequanghien4u@gmail.com <lequanghien4u@gmail.com>
2024-07-26 23:33:11 +0700

    add Handling multiple languages

--- shopapp/src/main/java/com/project/shopapp/controller/UserController.java
+++ shopapp/src/main/java/com/project/shopapp/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.project.shopapp.controller;
 
 import com.project.shopapp.models.User;
+import com.project.shopapp.responses.LoginResponse;
 import com.project.shopapp.services.IUserService;
+//import com.project.shopapp.configurations.LanguageConfig;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -12,14 +16,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.project.shopapp.dtos.*;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
 
 import java.util.List;
+import java.util.Locale;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("${api.prefix}/users")
 public class UserController {
     private final IUserService userService;
+    private final MessageSource messageSource;
+    private final LocaleResolver localeResolver;
     @PostMapping("/register")
     public ResponseEntity<?> createUser(@Valid @RequestBody UserDTO userDTO,
                                         BindingResult result){
@@ -43,20 +53,22 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@Valid @RequestBody UserLoginDTO userLoginDTO,
-                                        BindingResult result) {
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody UserLoginDTO userLoginDTO,
+                                               BindingResult result, HttpServletRequest request) {
         try {
-            if(result.hasErrors()){
-                List<String> errorMessages = result.getFieldErrors()
-                        .stream()
-                        .map(FieldError::getDefaultMessage)
-                        .toList();
-                return ResponseEntity.badRequest().body(errorMessages);
-            }
             String token = userService.login(userLoginDTO.getPhoneNumber(), userLoginDTO.getPassword());
-            return ResponseEntity.ok(token);
+            Locale locale = localeResolver.resolveLocale(request);
+            return ResponseEntity.ok(LoginResponse.builder()
+                    .message(messageSource
+                            .getMessage("user.login.login_successfully", null, locale))
+                    .token(token)
+                    .build());
         } catch(Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.badRequest().body(
+                    LoginResponse.builder()
+                            .message( e.getMessage())
+                            .build());
+
         }
 
     }
