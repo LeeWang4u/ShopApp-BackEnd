392ba9837be8de29d552efc0f5a4028db3fcf458
lequanghien4u@gmail.com <lequanghien4u@gmail.com>
2024-09-02 15:50:07 +0700

    add get user details

--- shopapp/src/main/java/com/project/shopapp/controllers/UserController.java
+++ shopapp/src/main/java/com/project/shopapp/controllers/UserController.java
@@ -1,9 +1,12 @@
 package com.project.shopapp.controllers;
 
+import com.project.shopapp.components.LocalizationUtils;
 import com.project.shopapp.models.User;
 import com.project.shopapp.responses.LoginResponse;
 import com.project.shopapp.services.IUserService;
+import com.project.shopapp.responses.UserResponse;
 //import com.project.shopapp.configurations.LanguageConfig;
+import com.project.shopapp.utils.MessageKeys;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +30,7 @@ public class UserController {
     private final IUserService userService;
     private final MessageSource messageSource;
     private final LocaleResolver localeResolver;
+    private final LocalizationUtils localizationUtils;
     @PostMapping("/register")
     public ResponseEntity<?> createUser(@Valid @RequestBody UserDTO userDTO,
                                         BindingResult result){
@@ -54,21 +58,47 @@ public class UserController {
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody UserLoginDTO userLoginDTO,
                                                 HttpServletRequest request) {
         try {
-            String token = userService.login(userLoginDTO.getPhoneNumber(), userLoginDTO.getPassword());
-            Locale locale = localeResolver.resolveLocale(request);
+            String token = userService.login(
+                    userLoginDTO.getPhoneNumber(),
+                    userLoginDTO.getPassword(),
+                    userLoginDTO.getRoleId() == null ? 1 : userLoginDTO.getRoleId()
+            );
+//            Locale locale = localeResolver.resolveLocale(request);
+//            return ResponseEntity.ok(LoginResponse.builder()
+//                    .message(messageSource
+//                            .getMessage("user.login.login_successfully", null, locale))
+//                    .token(token)
+//                    .build());
+//        } catch(Exception e){
+//            return ResponseEntity.badRequest().body(
+//                    LoginResponse.builder()
+//                            .message( e.getMessage())
+//                            .build());
+//
+//        }
             return ResponseEntity.ok(LoginResponse.builder()
-                    .message(messageSource
-                            .getMessage("user.login.login_successfully", null, locale))
+                    .message(localizationUtils.getLocalizedMessage(MessageKeys.LOGIN_SUCCESSFULLY))
                     .token(token)
                     .build());
-        } catch(Exception e){
+        } catch (Exception e) {
             return ResponseEntity.badRequest().body(
                     LoginResponse.builder()
-                            .message( e.getMessage())
-                            .build());
-
+                            .message(localizationUtils.getLocalizedMessage(MessageKeys.LOGIN_FAILED, e.getMessage()))
+                            .build()
+            );
         }
 
+    }
+
+    @PostMapping("/details")
+    public ResponseEntity<UserResponse> getUserDetails(@RequestHeader("Authorization") String token) {
+        try {
+            String extractedToken = token.substring(7); // Loại bỏ "Bearer " từ chuỗi token
+            User user = userService.getUserDetailsFromToken(extractedToken);
+            return ResponseEntity.ok(UserResponse.fromUser(user));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
 }
--- shopapp/src/main/java/com/project/shopapp/services/UserService.java
+++ shopapp/src/main/java/com/project/shopapp/services/UserService.java
@@ -1,12 +1,14 @@
 package com.project.shopapp.services;
 
 import com.project.shopapp.components.JwtTokenUtil;
+import com.project.shopapp.components.LocalizationUtils;
 import com.project.shopapp.dtos.UserDTO;
 import com.project.shopapp.exceptions.DataNotFoundException;
 import com.project.shopapp.models.Role;
 import com.project.shopapp.models.User;
 import com.project.shopapp.repositories.RoleRepository;
 import com.project.shopapp.repositories.UserRepository;
+import com.project.shopapp.utils.MessageKeys;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -26,6 +28,7 @@ public class UserService implements IUserService{
     private  final PasswordEncoder passwordEncoder;
     private final JwtTokenUtil jwtTokenUtil;
     private final AuthenticationManager authenticationManager;
+    private final LocalizationUtils localizationUtils;
     @Override
     @Transactional
     public User createUser(UserDTO userDTO) throws DataNotFoundException {
@@ -58,10 +61,31 @@ public class UserService implements IUserService{
     }
 
     @Override
-    public String login(String phoneNumber, String password) throws Exception{
+    public String login(String phoneNumber, String password, Long roleId) throws Exception{
+//        Optional<User> optionalUser = userRepository.findByPhoneNumber(phoneNumber);
+//        if(optionalUser.isEmpty()) {
+//            throw new DataNotFoundException("Invalid phone number / password");
+//        }
+//        //return optionalUser.get();//muốn trả JWT token ?
+//        User existingUser = optionalUser.get();
+//        //check password
+//        if (existingUser.getFacebookAccountId() == 0
+//                && existingUser.getGoogleAccountId() == 0) {
+//            if(!passwordEncoder.matches(password, existingUser.getPassword())) {
+//                throw new BadCredentialsException("Wrong phone number or password");
+//            }
+//        }
+//        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+//                phoneNumber, password,
+//                existingUser.getAuthorities()
+//        );
+//
+//        //authenticate with Java Spring security
+//        authenticationManager.authenticate(authenticationToken);
+//        return jwtTokenUtil.generateToken(existingUser);
         Optional<User> optionalUser = userRepository.findByPhoneNumber(phoneNumber);
         if(optionalUser.isEmpty()) {
-            throw new DataNotFoundException("Invalid phone number / password");
+            throw new DataNotFoundException(localizationUtils.getLocalizedMessage(MessageKeys.WRONG_PHONE_PASSWORD));
         }
         //return optionalUser.get();//muốn trả JWT token ?
         User existingUser = optionalUser.get();
@@ -69,8 +93,15 @@ public class UserService implements IUserService{
         if (existingUser.getFacebookAccountId() == 0
                 && existingUser.getGoogleAccountId() == 0) {
             if(!passwordEncoder.matches(password, existingUser.getPassword())) {
-                throw new BadCredentialsException("Wrong phone number or password");
+                throw new BadCredentialsException(localizationUtils.getLocalizedMessage(MessageKeys.WRONG_PHONE_PASSWORD));
             }
+        }
+        Optional<Role> optionalRole = roleRepository.findById(roleId);
+        if(optionalRole.isEmpty() || !roleId.equals(existingUser.getRole().getId())) {
+            throw new DataNotFoundException(localizationUtils.getLocalizedMessage(MessageKeys.ROLE_DOES_NOT_EXISTS));
+        }
+        if(!optionalUser.get().isActive()) {
+            throw new DataNotFoundException(localizationUtils.getLocalizedMessage(MessageKeys.USER_IS_LOCKED));
         }
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 phoneNumber, password,
@@ -80,5 +111,20 @@ public class UserService implements IUserService{
         //authenticate with Java Spring security
         authenticationManager.authenticate(authenticationToken);
         return jwtTokenUtil.generateToken(existingUser);
+    }
+
+    @Override
+    public User getUserDetailsFromToken(String token) throws Exception {
+        if(jwtTokenUtil.isTokenExpired(token)) {
+            throw new Exception("Token is expired");
+        }
+        String phoneNumber = jwtTokenUtil.extractPhoneNumber(token);
+        Optional<User> user = userRepository.findByPhoneNumber(phoneNumber);
+
+        if (user.isPresent()) {
+            return user.get();
+        } else {
+            throw new Exception("User not found");
+        }
     }
 }
