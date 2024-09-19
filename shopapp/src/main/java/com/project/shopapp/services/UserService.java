5281f3f50ecee104da77cb48fe49f2768e5317d4
lequanghien4u@gmail.com <lequanghien4u@gmail.com>
2024-09-19 09:22:45 +0700

    update in4 user

--- shopapp/src/main/java/com/project/shopapp/services/UserService.java
+++ shopapp/src/main/java/com/project/shopapp/services/UserService.java
@@ -2,6 +2,7 @@ package com.project.shopapp.services;
 
 import com.project.shopapp.components.JwtTokenUtil;
 import com.project.shopapp.components.LocalizationUtils;
+import com.project.shopapp.dtos.UpdateUserDTO;
 import com.project.shopapp.dtos.UserDTO;
 import com.project.shopapp.exceptions.DataNotFoundException;
 import com.project.shopapp.models.Role;
@@ -62,27 +63,6 @@ public class UserService implements IUserService{
 
     @Override
     public String login(String phoneNumber, String password, Long roleId) throws Exception{
-//        Optional<User> optionalUser = userRepository.findByPhoneNumber(phoneNumber);
-//        if(optionalUser.isEmpty()) {
-//            throw new DataNotFoundException("Invalid phone number / password");
-//        }
-//        //return optionalUser.get();//muốn trả JWT token ?
-//        User existingUser = optionalUser.get();
-//        //check password
-//        if (existingUser.getFacebookAccountId() == 0
-//                && existingUser.getGoogleAccountId() == 0) {
-//            if(!passwordEncoder.matches(password, existingUser.getPassword())) {
-//                throw new BadCredentialsException("Wrong phone number or password");
-//            }
-//        }
-//        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-//                phoneNumber, password,
-//                existingUser.getAuthorities()
-//        );
-//
-//        //authenticate with Java Spring security
-//        authenticationManager.authenticate(authenticationToken);
-//        return jwtTokenUtil.generateToken(existingUser);
         Optional<User> optionalUser = userRepository.findByPhoneNumber(phoneNumber);
         if(optionalUser.isEmpty()) {
             throw new DataNotFoundException(localizationUtils.getLocalizedMessage(MessageKeys.WRONG_PHONE_PASSWORD));
@@ -126,5 +106,51 @@ public class UserService implements IUserService{
         } else {
             throw new Exception("User not found");
         }
+    }
+
+    @Transactional
+    @Override
+    public User updateUser(Long userId, UpdateUserDTO updatedUserDTO) throws Exception {
+        // Find the existing user by userId
+        User existingUser = userRepository.findById(userId)
+                .orElseThrow(() -> new DataNotFoundException("User not found"));
+
+        // Check if the phone number is being changed and if it already exists for another user
+        String newPhoneNumber = updatedUserDTO.getPhoneNumber();
+        if (!existingUser.getPhoneNumber().equals(newPhoneNumber) &&
+                userRepository.existsByPhoneNumber(newPhoneNumber)) {
+            throw new DataIntegrityViolationException("Phone number already exists");
+        }
+
+        // Update user information based on the DTO
+        if (updatedUserDTO.getFullName() != null) {
+            existingUser.setFullName(updatedUserDTO.getFullName());
+        }
+        if (newPhoneNumber != null) {
+            existingUser.setPhoneNumber(newPhoneNumber);
+        }
+        if (updatedUserDTO.getAddress() != null) {
+            existingUser.setAddress(updatedUserDTO.getAddress());
+        }
+        if (updatedUserDTO.getDateOfBirth() != null) {
+            existingUser.setDateOfBirth(updatedUserDTO.getDateOfBirth());
+        }
+        if (updatedUserDTO.getFacebookAccountId() > 0) {
+            existingUser.setFacebookAccountId(updatedUserDTO.getFacebookAccountId());
+        }
+        if (updatedUserDTO.getGoogleAccountId() > 0) {
+            existingUser.setGoogleAccountId(updatedUserDTO.getGoogleAccountId());
+        }
+
+        // Update the password if it is provided in the DTO
+        if (updatedUserDTO.getPassword() != null
+                && !updatedUserDTO.getPassword().isEmpty()) {
+            String newPassword = updatedUserDTO.getPassword();
+            String encodedPassword = passwordEncoder.encode(newPassword);
+            existingUser.setPassword(encodedPassword);
+        }
+        //existingUser.setRole(updatedRole);
+        // Save the updated user
+        return userRepository.save(existingUser);
     }
 }
